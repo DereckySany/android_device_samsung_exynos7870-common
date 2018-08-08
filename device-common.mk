466205aa03d71c774013beec2608b879c092dad7
DarkLord1731 <darklord1731@gmail.com>
2018-08-08 22:08:51 +0530

    exynos7870-common: Build DRM HIDL packages for O

--- device-common.mk
+++ device-common.mk
@@ -53,6 +53,11 @@ PRODUCT_PACKAGES += \
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service
+
 # GPS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl
